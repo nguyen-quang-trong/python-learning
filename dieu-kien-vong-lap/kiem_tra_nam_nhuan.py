7f891cd6dfefb210cb0428950a3f3e1f1cb06d40
nguyen-quang-trong <nguyenquangtrong131006@gmail.com>
2026-08-18 12:59:38 +0700

    Create kiem_tra_nam_nhuan.py

---
+++ dieu-kien-vong-lap/kiem_tra_nam_nhuan.py
@@ -0,0 +1,5 @@
+year=int(input("Nhập vào 1 năm: ")) 
+if (year % 4 ==0 and year % 100 != 0) or year % 400 == 0: 
+    print("Năm ", year, " là năm nhuần") 
+else: 
+    print("Năm ", year, " không nhuần")
