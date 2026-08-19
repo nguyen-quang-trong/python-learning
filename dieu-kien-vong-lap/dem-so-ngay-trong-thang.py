a67f2ae8fcb80872fbb8c36e7f914f87506f2e8b
nguyen-quang-trong <nguyenquangtrong131006@gmail.com>
2026-08-19 09:54:28 +0700

    Thêm mô tả đề bài

--- dieu-kien-vong-lap/dem-so-ngay-trong-thang.py
+++ dieu-kien-vong-lap/dem-so-ngay-trong-thang.py
@@ -1,1 +1,5 @@
-
+"""
+Nhập vào 1 tháng, xuất tháng đó có bao nhiêu ngày.  
+1,3,5,7,8,10,12 ➔ có 31 ngày; 4,6,9,11 ➔ có 30 ngày 
+Nếu là tháng 2 thì yêu cầu nhập thêm năm. Năm nhuần thì tháng 2 có 29 ngày, không nhuần có 28 ngày
+"""
